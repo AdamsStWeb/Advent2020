473f708ea766ee71f022ce4e6298e171f47bd24d
AdamsStWeb <48396692+AdamsStWeb@users.noreply.github.com>
2020-12-01 19:29:10 -0500

    Update Day1.py
    Clean up

--- Day1.py
+++ Day1.py
@@ -1,24 +1,19 @@
-
+#Day1 Advent
 with open("Day1Input.txt") as f:
     numbers = f.readlines()
 numbers = [x.strip() for x in numbers] 
 
 #part 1
-for i in range(len(numbers)):
-    x = int(numbers[i])
-    for j in range(len(numbers)):
-        y = int(numbers[j])
-        if x + y == 2020:
-            print(x * y)
+for i in numbers:
+    for j in numbers:
+        if int(i) + int(j) == 2020:
+            print(int(i) * int(j))
     
 #part 2
-for i in range(len(numbers)):
-    x = int(numbers[i])
-    for j in range(len(numbers)):
-        y = int(numbers[j])
-        for k in range(len(numbers)):
-            z = int(numbers[k])
-            if x + y + z == 2020:
-                print(x * y * z)
+for i in numbers:
+    for j in numbers:
+        for k in numbers:
+            if int(i) + int(j) + int(k) == 2020:
+                print(int(i) * int(j) * int(k))
                 
         
