bee9c8877a5fa88943da71de2f0034cf09f9cd12
AdamsStWeb <48396692+AdamsStWeb@users.noreply.github.com>
2020-12-01 20:08:04 -0500

    Update Day1.py
    Added breaks

--- Day1.py
+++ Day1.py
@@ -4,16 +4,24 @@ with open("Day1Input.txt") as f:
 numbers = [x.strip() for x in numbers] 
 
 #part 1
+flag = False
 for i in numbers:
     for j in numbers:
         if int(i) + int(j) == 2020:
             print(int(i) * int(j))
-    
+            flag = True
+            break
+    if flag == True: break
+
 #part 2
+flag = False
 for i in numbers:
     for j in numbers:
         for k in numbers:
             if int(i) + int(j) + int(k) == 2020:
                 print(int(i) * int(j) * int(k))
-                
+                flag = True
+                break
+        if flag == True: break    
+    if flag == True: break            
         
